90ae1a061a89ed77be193b93e0d4168f85383f72
Cristian Garcia <cristiangarcianastar21@gmail.com>
2023-10-14 13:22:20 -0500

    ultimos ajustes de la estrategia de seguridad en strategy.ts

--- src/auth/strategy.ts
+++ src/auth/strategy.ts
@@ -24,33 +24,34 @@ export class AuthStrategy implements AuthenticationStrategy {
   async authenticate(request: Request): Promise<UserProfile | undefined> {
     let token = parseBearerToken(request);
     if (token) {
-      let idMenu: string = this.metadata[0].options![0];
+      let idPermissions: string = this.metadata[0].options![0];
       let accion: string = this.metadata[0].options![1];
       console.log(this.metadata);
 
-      const datos = {token: token, idMenu: idMenu, accion: accion};
+      const datos = {token: token, idPermissions: idPermissions, accion: accion};
       const urlValidarPermisos = `${ConfiguracionSeguridad.enlaceMicroservicioSeguridad}/validar-permisos`;
-      fetch(urlValidarPermisos, {
-        method: 'post',
-        body: JSON.stringify(datos),
-        headers: {'Content-Type': 'application/json', 'Authorization': `Bearer: ${token}`},
-      })
-        .then((res: any) => res.json())
-        .then((json: any) => {
-          console.log("Respuesta: ");
-          console.log(json);
-          console.log("Conectar con ms-seguridad")
-
-          let continuar: boolean = false;
-          if (continuar) {
-            let perfil: UserProfile = Object.assign({
-              permitido: "OK"
-            });
-            return perfil;
-          } else {
-            return undefined;
-          }
-        });
+      let res = undefined;
+      try{
+        await fetch(urlValidarPermisos, {
+          method: 'post',
+          body: JSON.stringify(datos),
+          headers: {'Content-Type': 'application/json'},
+        })
+          .then((res: any) => res.json())
+          .then((json: any) => {
+            res = json;
+          });
+        if (res) {
+          let perfil: UserProfile = Object.assign({
+            permitido: "OK"
+          });
+          return perfil;
+        } else {
+          return undefined;
+        }
+      } catch (e){
+          throw new HttpErrors[401]('No se tiene permisos sobre la accion a ejecutar.');
+      }
     }
     throw new HttpErrors[401]('No es posible ejecutar la acción por falta de un token.');
   }
